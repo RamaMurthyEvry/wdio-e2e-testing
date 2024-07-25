d6e773ae57e7605d2d4b706279c59e139850bac4
madhusub <madhushree.subrahmanyeswara@tietoevry.com>
2024-07-25 16:04:12 +0530

    Update flipcartHome.page.ts

--- src/pages/flipcart-pages/flipcartHome.page.ts
+++ src/pages/flipcart-pages/flipcartHome.page.ts
@@ -5,15 +5,7 @@ import flipcartHomeControls from '../flipcart-controls/flipcartHome.controls.ts'
 /**
  * sub page containing specific selectors and methods for a specific page
  */
-class FlipkartHomePage {   /**
-     * define selectors using getter methods
-     */
-    
-    
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+class FlipkartHomePage {  
     public async enterProductName(product: string){
         await (flipcartHomeControls.searchBox).waitForClickable()
         await (flipcartHomeControls.searchBox).setValue(product) 
