318942dec9d0cef678f932fc153e1f56500759c4
RamaMurthyEvry <165989450+RamaMurthyEvry@users.noreply.github.com>
2024-07-25 15:24:34 +0530

    Deleted old feature and updated product price verify logic

--- src/pages/amazon-pages/searchResult.page.ts
+++ src/pages/amazon-pages/searchResult.page.ts
@@ -18,30 +18,30 @@ export class SearchResultPage {
         await browser.url(newUrl)
     }
 
-    async verifyProductPrice( min:number, max:number) {
+    async verifyProductPrice(min: number, max: number) {
         try {
-          let locator=searchResultControl.productPrice
+            let locator = searchResultControl.productPrice
             let count = 0;
+            let lenth = await locator.length
             for (let i = 0; i < (await locator).length; i++) {
                 let prodDescription = (await locator[i].getText()).replace(/,/g, '');
                 let prodDescriptionNumber: number = +prodDescription;
-                if (prodDescriptionNumber>=min && prodDescriptionNumber<=max ) {
+                if (prodDescriptionNumber >= min && prodDescriptionNumber <= max) {
                     count = count + 1;
-                    console.log("Printing product name: " + prodDescription+" And Count: " +count)
+                    console.log("Printing product name: " + prodDescription + " And Count: " + count)
                 }
-    
             }
-            if (count == 0)
-                return false;
+            if (count == lenth)
+                return true;
             else
-                console.log("The count of the products which prices are in range is: "+count)
-            return true;
-        } catch (error) { 
-            console.error(`Error in getAllProductNames: ${error}`);
+                console.log("The count of the products which prices are in range is: " + count + " And the total products are " + lenth)
+            return false;
+        } catch (error) {
+                console.error(`Error in getAllProductNames: ${error}`);
         }
     }
 
-    
+
 
 }
 
