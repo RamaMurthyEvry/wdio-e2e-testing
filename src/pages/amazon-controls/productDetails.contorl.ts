24d64913848d8533ee329f5bcb073b1a3e88670d
kdangre1 <kunal.dangre@tietoevry.com>
2024-07-31 18:44:32 +0530

    scenario2_ final

--- src/pages/amazon-controls/productDetails.contorl.ts
+++ src/pages/amazon-controls/productDetails.contorl.ts
@@ -1,7 +1,7 @@
 class ProductDetailsContorl{
 
     public get productName() {return $('//span[@id="productTitle"]')}
-    public get productPrice() {return $('//span[3]/span[2]/span[2]')}
+    public get productPrice() {return $('//div[@id="corePriceDisplay_desktop_feature_div"]//*[@class="a-price-whole"]')}
     public get productQty() {return $('//div[@id="quantityLayoutLow_feature_div"]')}
 
 }
