0843fc4e449400c788a6d7f69a4907d6650a67c2
bindurani96 <bindu.s@tietoevry.com>
2024-08-01 20:49:38 +0530

    Updated to camelcase in control.ts and page.ts

--- src/pages/flipkart-controls/fkSortBy.control.ts
+++ src/pages/flipkart-controls/fkSortBy.control.ts
@@ -1,10 +1,10 @@
 import { $ } from '@wdio/globals';
 //#region Sort By Page Control
 export class fkSortByControl {
-    public get Popularity() { return $(`//div[@class='zg-M3Z' and contains(.,'Popularity')]`); }
-    public get LowtoHigh() { return $(`//div[@class='zg-M3Z' and contains(.,'Price -- Low to High')]`); }
-    public get HightoLow() { return $(`//div[@class='zg-M3Z' and contains(.,'Price -- High to Low')]`); }
-    public get NewestFirst() { return $(`//div[@class='zg-M3Z' and contains(.,'Newest First')]`); }
+    public get popularity() { return $(`//div[@class='zg-M3Z' and contains(.,'Popularity')]`); }
+    public get lowToHigh() { return $(`//div[@class='zg-M3Z' and contains(.,'Price -- Low to High')]`); }
+    public get highToLow() { return $(`//div[@class='zg-M3Z' and contains(.,'Price -- High to Low')]`); }
+    public get newestFirst() { return $(`//div[@class='zg-M3Z' and contains(.,'Newest First')]`); }
 //#endregion
 }
 export default new fkSortByControl();
