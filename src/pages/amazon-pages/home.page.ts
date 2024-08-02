a3f969226a09bdba211878ba245ad923ce7ccd7e
RamaMurthyEvry <165989450+RamaMurthyEvry@users.noreply.github.com>
2024-08-02 14:01:07 +0530

    Moved Dynamic locators to control page

--- src/pages/amazon-pages/home.page.ts
+++ src/pages/amazon-pages/home.page.ts
@@ -16,14 +16,13 @@ class HomePage {
     }
 
     async selectMainMenuItems(main_menu: string) {
-        await browser.$(`//div[@id='nav-xshop-container']//a[contains(text(),'${main_menu}')]`).click();
+        await (await homePageControl.menu(main_menu)).click()
     }
 
     async selectMenuAndSubMenu(category: string, subCategory: string) {
-        await browser.$(`//span[contains(text(),'${category}')]`).moveTo();
+        await (await homePageControl.Filter(category)).moveTo();
         await browser.pause(3000)
-        const subMenuOptions = await browser.$(`//div[@class='mega-menu']//h3[text()='${subCategory}'] | //div[@class='mega-menu']//h3//a[text()='${subCategory}']`)
-        await browser.$(subMenuOptions).click()
+        await (await homePageControl.subFilter(subCategory)).click();
         const isPresent = await isTextPresentInLocator(homePageControl.searchDropDown, subCategory);
         expect(isPresent).toBe(true);
     }
